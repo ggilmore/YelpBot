63dba72d6a1707f9e84d6dbc3b70217ecf38fe43
Lyn Nagara <lyn.nagara@gmail.com>
2015-03-23 22:09:20 -0400

    ignore target folder

--- src/main/scala/Object.scala
+++ src/main/scala/Object.scala
@@ -9,7 +9,7 @@ object Junk extends App {
 
 
 
-  """-d "type=private" -d  "to=ggilmore@mit.edu" -d "content=I come not, friends, to steal away your hearts." """
+  // """-d "type=private" -d  "to=ggilmore@mit.edu" -d "content=I come not, friends, to steal away your hearts." """
 
 
   println(Http("https://api.zulip.com/v1/messages").auth("yelpbot-bot@students.hackerschool.com", "NK3ilaGgyV9TxCikfEBbJVmBUKzNYKMx").postForm(Seq("type" -> "private", "to" -> "ggilmore@mit.edu", "content" ->"I come not, friends, to steal away your hearts." )).asString)
