860a3391ae01526f53db9add55cf663ce5a69a1d
Lyn Nagara <lyn.nagara@gmail.com>
2015-03-24 14:48:20 -0400

    zulip message requester

--- src/main/scala/ZulipCommunicators/ZulipMessageRequester.scala
+++ src/main/scala/ZulipCommunicators/ZulipMessageRequester.scala
@@ -3,15 +3,33 @@ import scalaj.http.Http
 import ZulipMessageSender.BOT_NAME
 
 import MessageClasses.ZulipInboundMessage
+import Queues._
 
 /**
  * Created by gmgilmore on 3/24/15.
  */
-object ZulipMessageRequester {
+object ZulipMessageRequester extends App {
 
   private val ZULIP_ADDR_REGISTER ="https://api.zulip.com/v1/register"
 
   private val ZULIP_ADDR_EVENTS = "https://api.zulip.com/v1/events"
+
+
+  private val requester:Thread = new Thread(new Runnable{
+    def run():Unit = {
+      while(true){
+        val msg = getLatestEvents()
+        Queues.putOnZulipInQueue(msg)
+        Thread.sleep(5000)
+      }
+    }
+  })
+
+  requester.start
+
+ val msg = Queues.takeFromZulipInQueue
+ println(msg)
+
 
 
   def registerQueue:ZulipInboundMessage= ZulipInboundMessage(Http(ZULIP_ADDR_REGISTER).auth(BOT_NAME, SecretKeys.ZULIP_BOT_KEY).postData(s"""event_types=["message"]""").asString)
