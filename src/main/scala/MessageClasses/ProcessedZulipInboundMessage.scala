7dab68edfdf77a94dfe10881bf3897b59a61fb22
Lyn Nagara <lyn.nagara@gmail.com>
2015-03-24 16:14:28 -0400

    create queue message classes

--- src/main/scala/MessageClasses/ProcessedZulipInboundMessage.scala
+++ src/main/scala/MessageClasses/ProcessedZulipInboundMessage.scala
@@ -4,9 +4,16 @@ package MessageClasses
  * Created by gmgilmore on 3/24/15.
  */
 sealed trait ProcessedZulipInboundMessage {
-
+  val contents:String
+  val headers:String
+  val statusCode:String
 }
 
-case class QueueMessage(contents:String) extends ProcessedZulipInboundMessage
+case class QueueMessage(contents:String, headers: String, statusCode: String, 
+  target:String, subject:String="", isPrivate:Boolean=false, id:String) extends ProcessedZulipInboundMessage
 
-case class MessageSendingSuccessful(contents:String) extends ProcessedZulipInboundMessage
+case class MessageSendingSuccessful(contents:String, headers: String, statusCode: String,
+  message:String, result:Boolean, id:String) extends ProcessedZulipInboundMessage
+
+case class RequestEventQueue(contents:String, headers: String, statusCode: String,
+  message:String, lastEventId:String, result:Boolean, queueId:String) extends ProcessedZulipInboundMessage
