0dcb4237f1984b60bb97fe25db971b1a891e506e
Geoffrey Gilmore <ggilmore@mit.edu>
2015-03-24 12:18:04 -0400

    implemented getLatestEvents

--- src/main/scala/Object.scala
+++ src/main/scala/Object.scala
@@ -9,24 +9,15 @@ import DefaultJsonProtocol._
 import scala.sys.process._
 object Junk extends App {
 
-
-
-
-  """-d "type=private" -d  "to=ggilmore@mit.edu" -d "content=I come not, friends, to steal away your hearts." """
+  val BOT_NAME = "yelpbot-bot@students.hackerschool.com"
 
   val ZULIP_ADDR_MESSAGES = "https://api.zulip.com/v1/messages"
-
-  val BOT_NAME = "yelpbot-bot@students.hackerschool.com"
 
   val ZULIP_ADDR_REGISTER ="https://api.zulip.com/v1/register"
 
   val ZULIP_ADDR_EVENTS = "https://api.zulip.com/v1/events"
 
-
-
-
 //  println(Http("https://api.zulip.com/v1/messages").auth("yelpbot-bot@students.hackerschool.com", "NK3ilaGgyV9TxCikfEBbJVmBUKzNYKMx").postForm(Seq("type" -> "private", "to" -> "ggilmore@mit.edu", "content" ->"I come not, friends, to steal away your hearts." )).asString)
-//
 
   def sendToZulip(addr:String = ZULIP_ADDR_MESSAGES, authName:String = BOT_NAME,
                   authPW:String = SecretKeys.ZULIP_BOT_KEY, isPrivate:Boolean = false,
@@ -37,9 +28,13 @@ object Junk extends App {
 
   def registerQueue:HttpResponse[String] = Http(ZULIP_ADDR_REGISTER).auth(BOT_NAME, SecretKeys.ZULIP_BOT_KEY).postData(s"""event_types=["message"]""").asString
 
-//  def getLatestEvents:HttpResponse[String] = Http(ZULIP_ADDR_EVENTS).auth(BOT_NAME, SecretKeys.ZULIP_BOT_KEY).params()
+  def getLatestEvents(queueID:String = SecretKeys.ZULIP_BOT_KEY, eventQueueID:String = SecretKeys.QUEUE_ID, lastEventID: String = "0") :HttpResponse[String] =
+    Http(ZULIP_ADDR_EVENTS).auth(BOT_NAME, SecretKeys.ZULIP_BOT_KEY).
+      params(Seq(("queue_id",eventQueueID), ("last_event_id",lastEventID))).asString
 
+//  println(sendToZulip(target="lyn.nagara@gmail.com", content = "test", isPrivate = true))
 //  println(registerQueue)
+  println(getLatestEvents())
 
 
 }
